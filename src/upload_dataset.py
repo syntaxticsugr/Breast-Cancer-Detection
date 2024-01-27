edef51859150f8650fd591667b713c5f2a78dc57
syntaxticsugr <132896073+syntaxticsugr@users.noreply.github.com>
2024-01-27 13:55:02 +0530

    use new labels csv with image dir

--- src/upload_dataset.py
+++ src/upload_dataset.py
@@ -5,22 +5,7 @@ import boto3
 
 
 
-def images_path_df(dataset_dir):
-
-    images_path_df_rows = []
-
-    for (dirpath, _, filenames) in os.walk(dataset_dir, topdown=True):
-
-        for filename in filenames:
-
-            images_path_df_rows.append({"full_path":f'{dirpath}/{filename}'})
-
-    image_paths_df = pd.DataFrame(data=images_path_df_rows)
-    return image_paths_df
-
-
-
-def upload_dataset(aws_access_key_id, aws_secret_access_key, region_name, bucket_name, dataset_dir, start_index):
+def upload_dataset(aws_access_key_id, aws_secret_access_key, region_name, bucket_name, labels_csv):
 
     s3_client = boto3.client(
         service_name = 's3',
@@ -29,28 +14,30 @@ def upload_dataset(aws_access_key_id, aws_secret_access_key, region_name, bucket
         aws_secret_access_key = aws_secret_access_key
     )
 
-    images_path = images_path_df(dataset_dir)
+    images_path = pd.read_csv(labels_csv)
 
     total_time = 0
-    items_remaining = (len(images_path)-start_index)
+    items_remaining = len(images_path)
     print(f"\033[A\033[A\n\nDone: 0        Remaining: {items_remaining}        Ellapsed Time: 0        Estimated Remaining Time: --:--:--        \n")
 
-    for index in range(start_index, len(images_path)):
+    for index in range(0, len(images_path)):
 
         start_time = time.time()
 
-        image_path = images_path['full_path'][index]
-        image_name = os.path.basename(image_path)
+        image_dir = images_path['dir'][index]
+        image_name = images_path['image'][index]
+
+        fullpath = f'{image_dir}/{image_name}'
 
         s3_client.upload_file(
-            image_path,
+            fullpath,
             bucket_name,
-            f'dataset/kaggle/{image_name}'
+            fullpath
         )
         
         total_time += (time.time() - start_time)
         items_remaining -= 1
-        print(f"\033[A\033[ADone: {(index+1)-start_index}         Remaining: {items_remaining}        Ellapsed Time: {time.strftime('%H:%M:%S', time.gmtime(total_time))}        Estimated Remaining Time: {time.strftime('%H:%M:%S', time.gmtime((items_remaining*(total_time/((index+1)-start_index)))))}        \n")
+        print(f"\033[A\033[ADone: {index+1}         Remaining: {items_remaining}        Ellapsed Time: {time.strftime('%H:%M:%S', time.gmtime(total_time))}        Estimated Remaining Time: {time.strftime('%H:%M:%S', time.gmtime((items_remaining*(total_time/(index+1)))))}        \n")
 
 
 
@@ -62,7 +49,6 @@ if __name__ == "__main__":
     region_name = 'ap-southeast-2'
     bucket_name = 'bcd-sugrbucket'
 
-    dataset_dir = r'dataset'
-    start_index = 0
+    labels_csv = r'src/labels/labels.csv'
 
-    upload_dataset(aws_access_key_id, aws_secret_access_key, region_name, bucket_name, dataset_dir, start_index)
+    upload_dataset(aws_access_key_id, aws_secret_access_key, region_name, bucket_name, labels_csv)
