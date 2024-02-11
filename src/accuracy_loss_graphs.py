73db2eaf485d3e9ca28bc1b1b140150ce18c5ed7
syntaxticsugr <132896073+syntaxticsugr@users.noreply.github.com>
2024-02-11 09:15:31 +0530

    accuracy and loss graphs

---
+++ src/accuracy_loss_graphs.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+
+def accuracy_loss_graphs(log_csv):
+
+    df = pd.read_csv(log_csv)
+
+    plt.style.use('ggplot')
+
+    plt.subplot(1,2,1)
+    plt.plot(df['accuracy'])
+    plt.plot(df['val_accuracy'])
+    plt.title("Accuracy")
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.legend(['train accuracy', 'validation accuracy'], loc='best', prop={'size': 12})
+
+    plt.subplot(1,2,2)
+    plt.plot(df['loss'])
+    plt.plot(df['val_loss'])
+    plt.title("Loss")
+    plt.xlabel('Epoch')
+    plt.ylabel('Loss')
+    plt.legend(['train loss', 'validation loss'], loc='best', prop={'size': 12})
+
+    plt.show()
+
+
+
+if __name__ == "__main__":
+
+    log_csv = r'saved-models/bcd-final/bcd-final-fit-log.csv'
+
+    accuracy_loss_graphs(log_csv)
--- src/misc/check_image_size.py
+++ src/misc/check_image_size.py
@@ -1,10 +1,16 @@
 import pandas as pd
 from skimage import io
+from utils.create_directory import create_directory
 from utils.save_image import save_image
+from skimage.exposure import is_low_contrast
 
 
 
 def check_image_size(labels_csv, save_dir):
+
+    save_dir = f'{save_dir}/not50x50'
+
+    create_directory(save_dir)
 
     labels_csv_df = pd.read_csv(labels_csv)
 
@@ -20,10 +26,32 @@ def check_image_size(labels_csv, save_dir):
 
 
 
+def check_low_contrast_image(labels_csv, save_dir):
+
+    save_dir = f'{save_dir}/low-contrast'
+
+    create_directory(save_dir)
+
+    labels_csv_df = pd.read_csv(labels_csv)
+
+    for index in range(0, len(labels_csv_df)):
+
+        image_dir = labels_csv_df['dir'][index]
+        image_name = labels_csv_df['image'][index]
+
+        image = io.imread(f"{image_dir}/{image_name}")
+
+        if (is_low_contrast(image)):
+            save_image(image, image_name, save_dir)
+
+
+
 if __name__ == '__main__':
 
     labels_csv = r'src/labels/labels.csv'
 
-    save_dir = r'not50x50'
+    save_dir = r'error-images'
 
     check_image_size(labels_csv, save_dir)
+
+    check_low_contrast_image(labels_csv, save_dir)
